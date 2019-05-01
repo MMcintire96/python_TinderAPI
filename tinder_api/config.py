b8f3be8c213e93570fd53d678ff29a518f644364
michael <michaeljamesmcintire@gmail.com>
2019-05-01 17:58:33 -0600

    removed __pyshit__

--- tinder_api/config.py
+++ tinder_api/config.py
@@ -9,7 +9,8 @@ fb_password = "fb_pass"
 #fb_auth_token = fb_auth.get_fb_access_token(fb_email, fb_password)
 #fb_user_id = fb_auth.get_fb_id(fb_auth_token)
 
-tinder_token = "YOUR TOKEN HERE"
+tinder_token = "33660983-b36a-4882-b081-cad76ae4b03d"
+
 headers = {
     'app_version': '6.9.4',
     'platform': 'ios',
