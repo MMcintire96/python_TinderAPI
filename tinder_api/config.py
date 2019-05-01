84945e225f9f2d9402a289429594c475b56f029c
Michael McIntire <44066543+MMcintire96@users.noreply.github.com>
2019-05-01 17:59:32 -0600

    Update config.py

--- tinder_api/config.py
+++ tinder_api/config.py
@@ -9,7 +9,7 @@ fb_password = "fb_pass"
 #fb_auth_token = fb_auth.get_fb_access_token(fb_email, fb_password)
 #fb_user_id = fb_auth.get_fb_id(fb_auth_token)
 
-tinder_token = "33660983-b36a-4882-b081-cad76ae4b03d"
+tinder_token = "YOUR TOKEN HERE"
 
 headers = {
     'app_version': '6.9.4',
