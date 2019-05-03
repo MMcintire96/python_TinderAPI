caff8310eeae080000f77f5e7b3559eb8c62e182
michael <michaeljamesmcintire@gmail.com>
2019-05-03 13:34:46 -0600

    new wrapper and example update

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
 import tinder_api.session
 import itertools
-
+from datetime import datetime
 sess = tinder_api.session.Session() # inits the session
 
 print("My _id is %s" %sess.get_id())
 
 #sess.update_profile(bio="VIM is the best")
 
-for user in itertools.islice(sess.yield_users(), 3):
+for user in itertools.islice(sess.yield_users(), 10):
     print(user.name) # prints the name of the user see __init__
+    # How to check if it exists, if it doesnt, it returns <MisssingValue>
+    if user.bio is not "<MissingValue>":
+        print(user.bio)
     print(user.like()) # returns false if not a match
 
 for match in sess.yield_matches():
--- tinder_api/utils/wrapper.py
+++ tinder_api/utils/wrapper.py
@@ -1,9 +1,11 @@
 import json
-
+import pprint
 
 #old version of the wrapper -- thanks to https://github.com/sharkbound
-class NoValue:
+
+class MissingValue:
     __slots__ = ()
+    VALUE = '<MissingValue>'
 
     def __getattr__(self, item):
         return self
@@ -14,23 +16,44 @@ class NoValue:
     def __iter__(self):
         yield from ()
 
+    def __contains__(self, item):
+        return False
+
     def __bool__(self):
         return False
 
+    def __repr__(self):
+        return self.VALUE
+
     def __str__(self):
-        return '<NoValue>'
+        return self.VALUE
 
 
-NOVALUE = NoValue()
+MISSING_VALUE = MissingValue()
 
 
 class JsonWrapper:
     def __init__(self, data, iter_keys_only=False):
+        """
+        :param data: the JSON data to wrap, can be a list, tuple, or dict
+        :param iter_keys_only:
+            sets the behavior when iterating over the wrapper when it contains a dict.
+            False means that it will iterate over dict KEY/VALUE pairs,
+                aka {'name': 'john doe'} would iterate as: ('name', 'john does');
+            True means the it will only iterate over dict KEYS,
+                aka {'name': 'john doe'} would iterate as: ('name')
+        """
         self.iter_keys_only = iter_keys_only
         self._data = data
 
-    def _wrap_if_needed(self, key, value, no_key=False):
-        if value is not NOVALUE and isinstance(value, (dict, list, tuple)):
+    def pretty(self):
+        """
+        returns a pretty printed version of the data this wrapper holds
+        """
+        return pprint.pformat(self._data)
+
+    def _wrap(self, key, value, no_key=False):
+        if value is not MISSING_VALUE and isinstance(value, (dict, list, tuple)):
             value = self.__class__(value, iter_keys_only=self.iter_keys_only)
             if not no_key:
                 self._data[key] = value
@@ -42,9 +65,9 @@ class JsonWrapper:
             value = self._data.get(item)
         elif (isinstance(self._data, (list, tuple)) and
               isinstance(item, int) and
-              item in range(-len(self._data), len(self._data))):
+              -len(self._data) <= item < len(self._data)):
             value = self._data[item]
-        return self._wrap_if_needed(item, value if value is not None else NOVALUE)
+        return self._wrap(item, value if value is not None else MISSING_VALUE)
 
     def __getattr__(self, item):
         if item in self.__dict__:
@@ -56,28 +79,56 @@ class JsonWrapper:
         return self._get_value(item)
 
     def __contains__(self, item):
+        if isinstance(item, int) and isinstance(self._data, (list, tuple)):
+            l = len(self._data)
+            return -l <= item < l
+
         return item in self._data
 
     def __bool__(self):
-        return bool(self.__dict__)
+        return bool(self._data)
 
     def __iter__(self):
         if isinstance(self._data, dict):
-            if self.iter_keys_only:
-                yield from self._data
-            else:
-                yield from self._data.items()
+            yield from (self._data if self.iter_keys_only else self._data.items())
         elif isinstance(self._data, (list, tuple)):
-            yield from (self._wrap_if_needed(None, v, no_key=True) for v in self._data)
+            yield from (self._wrap(None, v, no_key=True) for v in self._data)
         else:
             try:
                 yield from self._data
-            except:
+            except TypeError:
                 yield from ()
 
+    def __repr__(self):
+        return f'<JsonWrapper {self._data.__class__.__name__}>'
+
     def __str__(self):
-        if isinstance(self._data, (list, tuple)):
-            return str(self._data)
-        elif isinstance(self._data, dict):
-            return str(tuple(self._data))
-        return str(self._data)
+        if isinstance(self._data, dict):
+            return pprint.pformat(tuple(self._data))
+
+        return pprint.pformat(self._data)
+
+
+def has_value(value):
+    """
+    verifies that a value is not MISSING_VALUE
+    True return means it is not MISSING_VALUE
+    False return means that the value was MISSING_VALUE
+    :param value: the value to check if it is not MISSING_VALUE
+    """
+    return value is not MISSING_VALUE
+
+# def test():
+#     data = JsonWrapper({
+#         'bio': r'¯\_(ツ)_/¯',
+#         'photos': [
+#             {
+#                 'url': 'url here'
+#             }
+#         ]
+#     })
+#
+#     assert data.photos[0].url == 'url here'
+#     assert data.photos[100].url is MISSING_VALUE
+#     assert data.bio == r'¯\_(ツ)_/¯'
+#     assert data.missing is MISSING_VALUE
